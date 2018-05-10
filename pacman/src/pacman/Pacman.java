e1f5511b0fc415493b53e0784e1f261f7d097baf
standean <39045432+standean@users.noreply.github.com>
2018-05-10 14:59:29 -0700

    Add files via upload

--- pacman/src/pacman/Pacman.java
+++ pacman/src/pacman/Pacman.java
@@ -1,35 +1,56 @@
 package pacman;
 
 import java.awt.Graphics;
+import java.awt.Image;
 
 public class Pacman {
-	public enum Direction{North,South,East,West};
+	public enum Direction{NORTH,SOUTH,EAST,WEST};
 	private int lives;
 	private Direction dir;
 	private int score;
 	private int x;
 	private int y;
 	
+	private String fileName="images/pacman";
+	
 	public Pacman(){
 		lives=3;
 		score=0;
-		dir=Direction.East;
-		x=0;		// start in the lower left corner, facing right
+		dir=Direction.EAST;
+		x=0;	// start in the lower left corner, facing right
 		//y= height of board
 		
 	}
 	
-	public void eat(int foodValue) {	// in the food class, have a food.getValue() method that returns an int
-		score+=foodValue;
+	public void changeScore(int value) {
+		score+=value;
 	}
 	
-	public void move() {
-		
+	public void addLives(int num) {
+		lives+=num;
 	}
 	
-	public void draw(Graphics g) {
-		
+	public void subtractLives(int num) {
+		addLives(-num);
 	}
+	
+	public void moveAndDraw(Graphics g) {
+		// check to make sure it's not facing a wall
+		// add walls on the edges of the board so we don't need to do a bounds check
+		if (facingEast())
+			x+=3;
+		else if (facingWest())
+			x-=3;
+		else if (facingNorth())
+			y-=3;
+		else
+			y+=3;
+		
+		fileName=fileName + dir.toString() + ".png";	// example: pacmanNorth.png
+		Image img = null;
+		g.drawImage(img, x, y, null);
+	}
+	
 	
 	public Direction getDirection() {
 		return dir;
@@ -47,6 +68,19 @@ public class Pacman {
 		return y;
 	}
 	
+	
+	public boolean facingEast() {
+		return dir==Direction.EAST;
+	}
+	public boolean facingWest() {
+		return dir==Direction.WEST;
+	}
+	public boolean facingNorth() {
+		return dir==Direction.NORTH;
+	}
+	public boolean facingSouth() {
+		return dir==Direction.SOUTH;
+	}
 	
 	
 
