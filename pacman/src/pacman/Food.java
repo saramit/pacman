774209d9335de892d2d94abae630effb642b06f0
saramit <39068876+saramit@users.noreply.github.com>
2018-05-11 15:10:42 -0700

    Update Food.java

--- pacman/src/pacman/Food.java
+++ pacman/src/pacman/Food.java
@@ -1,5 +1,25 @@
 package pacman;
 
 public abstract class Food {
+	private abstract int points;
+	private abstract int xCoord;
+	private abstract int yCoord;
+	private boolean isEaten;
 
+	public void visible (Graphics g){
+		if (isEaten == false){
+			g.setVisible(true);
+		}
+		else{
+			g.setVisible(false);
+		}
+	}
+	
+	public void ifEaten {
+		isEaten = true;
+		points = 0;
+		visible(g);
+	}
+	
+	
 }
