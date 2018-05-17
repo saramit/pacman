0944232c4276368d4f9a3eb29e9f40ea2708fcdf
saramit <39068876+saramit@users.noreply.github.com>
2018-05-17 14:03:44 -0700

    Add files via upload

---
+++ pacman/src/pacman/pacman/PinkGhost.java
@@ -0,0 +1,12 @@
+package pacman;
+
+public class PinkGhost extends Ghost {
+
+	@Override
+	public void doObjective() {
+		// TODO Auto-generated method stub
+		
+	}
+	// aims in front of pacman's mouth
+
+}
