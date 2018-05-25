b486d2916932e3e667c76e654bb61066d9997e1c
standean <39045432+standean@users.noreply.github.com>
2018-05-25 15:09:57 -0700

    Add files via upload

--- pacman/src/pacman/Driver.java
+++ pacman/src/pacman/Driver.java
@@ -3,6 +3,7 @@ package pacman;
 public class Driver {
 	Pacman p;
 	World w;
+	Grid g;
 	
 
 	public static void main(String[] args) {
@@ -15,9 +16,45 @@ public class Driver {
 	
 	public void start () {
 		p = new Pacman();
+		w = new World();
+		g = w.getGrid();
+		JPanel panel = new JPanel();
+		
+		// swing timer tutorial:
+		// https://docs.oracle.com/javase/tutorial/uiswing/misc/timer.html
+		ActionListener listener = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// do stuff
+			}
+		};
+		timer = new Timer(100, listener);
+		timer.start();
+		
+		
+		// key binding tutorial:
+		// https://docs.oracle.com/javase/tutorial/uiswing/misc/keybinding.html
+		panel.getInputMap().put(KeyStroke.getKeyStroke("up"));
+		panel.getInputMap().put(KeyStroke.getKeyStroke("down"));
+		panel.getInputMap().put(KeyStroke.getKeyStroke("left"));
+		panel.getInputMap().put(KeyStroke.getKeyStroke("right"));
+		
+		panel.getActionMap().put("up", new AbstractAction() {
+			p.changeDirection(w,Direction.NORTH);
+		});
+		panel.getActionMap().put("down", new AbstractAction() {
+			p.changeDirection(w,Direction.SOUTH);
+		});
+		panel.getActionMap().put("left", new AbstractAction() {
+			p.changeDirection(w,Direction.WEST);
+		});
+		panel.getActionMap().put("right", new AbstractAction() {
+			p.changeDirection(w,Direction.EAST);
+		});
 		
 		
 	}
+	
+	
 	
 	public Pacman getPacman() {
 		return p;
